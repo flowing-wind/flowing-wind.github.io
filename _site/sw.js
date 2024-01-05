9407da063533f0adc855c29a1bc47d07d49cbeb9
flowing_wind <p19y28t37h46o5n@gmail.com>
2024-01-05 23:07:32 +0800

    upgrade config

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704464511';
+const cacheName = 'chirpy-1704467203';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
