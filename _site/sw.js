8451594c01c060f997df1e13ea170a60d4dc3b7e
flowing_wind <p19y28t37h46o5n@gmail.com>
2024-01-06 13:04:12 +0800

    rewrite about.md

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704468298';
+const cacheName = 'chirpy-1704517387';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
